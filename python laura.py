0ab8f2a088435f42c8b43fbf066863d7c8eec96d
lauraroserubin <lauraroserubin@gmail.com>
2024-10-04 16:11:28 +0530

    Add files via upload

---
+++ python laura.py
@@ -0,0 +1,24 @@
+from functools import reduce
+'''
+print("hello \n world" )
+if 5 > 2:
+ print("five is greater than two")
+ print("five is greater than two")
+ x=5
+ print("5")
+ x = str(3)
+ y = int(3)
+ z = float(3)
+ print(x,y,z)
+ print(type(x))
+ print(type(y))
+ print(type(z))
+ x = "python "
+ y = "is "
+ z = "awesome"
+ print(x+y+z)
+'''
+username = input("enter name")
+print("my name is ",  username )
+b = "hello world"
+print(b[2:5])
