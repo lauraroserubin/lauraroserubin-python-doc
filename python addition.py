f9be0ede2012f54c68b0c525096bde2163079b60
lauraroserubin <lauraroserubin@gmail.com>
2024-10-18 15:57:26 +0530

    Add files via upload

---
+++ python addition.py
@@ -0,0 +1,8 @@
+number=int(input("enter a number"))
+number1=int(input("enter a number"))
+print("sum=",number+number1,",Division",number/number1)
+print("is number greater than number1?:",number>number1)
+print(" are number and number1 equal",number==number1)
+print("number greater than zero and number1 greater than zero",number>0 and number1>0)
+print("logical AND:",number>0 and number1>0)
+print("logical OR :",number>0 or number1<0)
