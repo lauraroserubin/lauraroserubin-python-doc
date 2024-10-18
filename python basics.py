948592828c58de9bf5939f306de31188a85e37d5
lauraroserubin <lauraroserubin@gmail.com>
2024-10-18 15:11:27 +0530

    Add files via upload

---
+++ python basics.py
@@ -0,0 +1,14 @@
+'''
+Author:Laura Rose Rubin
+Date:18-10-2024
+Description:program that stores a string in a variable. Extract a specific part of the string (substring) and then concatenate it with another string. Finally, display the new string.
+'''
+
+
+
+
+str_b=str_var[6:11]
+print(str_b)
+str_c=" everyone!"
+str_a=str_b+str_c
+print(str_a)
