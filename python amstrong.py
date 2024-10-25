f57c80f3d58fccdfa8fb5c8d6f23b7cebc44af4e
lauraroserubin <lauraroserubin@gmail.com>
2024-10-25 16:25:37 +0530

    Add files via upload

---
+++ python amstrong.py
@@ -0,0 +1,18 @@
+'''
+Author:Laura Rose Rubin
+Date:25-10-2024
+'''
+
+
+
+num=int(input("enter a number"))
+
+sum=0
+while(num>0):
+    r=sum%10
+    sum = sum+r**3
+    num=num//10
+if(sum==num):
+    print("it is an armstrong number")
+else:
+    print("it is not an armstrong number")
