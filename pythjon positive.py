e1cf4b4b6d8783b6b6549ac1a16b99264acd1315
lauraroserubin <lauraroserubin@gmail.com>
2024-10-25 14:34:15 +0530

    Add files via upload

---
+++ pythjon positive.py
@@ -0,0 +1,13 @@
+'''
+Author:Laura Rose Rubin
+Date:25-10-2024
+Description:python program to find if number is positive or negative
+'''
+
+
+
+num_1=int(input("enter a number"))
+if num_1>0:
+    print(num_1," is positive")
+else:
+    print(num_1," is negative")
