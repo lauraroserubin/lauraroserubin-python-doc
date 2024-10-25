ca07d17bce4562742a7b63acfcb677fc691902ce
lauraroserubin <lauraroserubin@gmail.com>
2024-10-25 16:12:06 +0530

    Add files via upload

---
+++ python twinkle.py
@@ -0,0 +1,9 @@
+'''
+Author:Laura Rose Rubin
+Date:18-10-2024
+Description:Write a Python program to print the following string in a specific format
+'''
+
+
+
+print("Twinkle, twinkle, little star,\n How I wonder what you are!\n\t\t\t Up above the world so high,\n Like a diamond in the sky. \nTwinkle, twinkle, little star,\n\t\t How I wonder what you  are")
