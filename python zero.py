06c1d26c70fe52a539200d83b2eedece1805c254
lauraroserubin <lauraroserubin@gmail.com>
2024-10-25 14:41:28 +0530

    Add files via upload

---
+++ python zero.py
@@ -0,0 +1,15 @@
+'''
+Author:Laura Rose Rubin
+Date:25-10-2024
+Description:python program to check whether a number is positive,negative or zero
+'''
+
+
+
+num_1=int(input("enter a number"))
+if num_1>0:
+    print(num_1," is positive")
+elif num_1<0:
+    print(num_1," is negative")
+else:
+    print(num_1," is zero")
