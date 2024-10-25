4e7eae4dd233330f5f62e6f99c444e7c97105e38
lauraroserubin <lauraroserubin@gmail.com>
2024-10-25 14:21:44 +0530

    Add files via upload

---
+++ python num.py
@@ -0,0 +1,8 @@
+num_1=int(input("enter first number"))
+num_2=int(input("enter second number"))
+if num_1>num_2:
+    print(num_1," is greater than ",num_2)
+else:
+    print(num_2," is greater than ",num_1)
+
+
